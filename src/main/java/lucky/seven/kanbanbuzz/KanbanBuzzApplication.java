7cd088e55d987d19113d143417b3ccb81e7cc9de
llocr <36-96@naver.com>
2024-07-10 12:51:43 +0900

    init : 프로젝트 초기 설정

---
+++ src/main/java/lucky/seven/kanbanbuzz/KanbanBuzzApplication.java
@@ -0,0 +1,13 @@
+package lucky.seven.kanbanbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBuzzApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBuzzApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/lucky/seven/kanbanbuzz/KanbanBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package lucky.seven.kanbanbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBuzzApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
