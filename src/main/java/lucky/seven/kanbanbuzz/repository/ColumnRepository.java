820d5a62495163d3fd1a2513b09f5a254efc81bb
llocr <36-96@naver.com>
2024-07-12 15:50:53 +0900

    refactor/#13 : 로직 추가

--- src/main/java/lucky/seven/kanbanbuzz/repository/ColumnRepository.java
+++ src/main/java/lucky/seven/kanbanbuzz/repository/ColumnRepository.java
@@ -1,5 +1,6 @@
 package lucky.seven.kanbanbuzz.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import lucky.seven.kanbanbuzz.dto.ColumnResponseDto;
 import lucky.seven.kanbanbuzz.entity.Column;
 
 @Repository
