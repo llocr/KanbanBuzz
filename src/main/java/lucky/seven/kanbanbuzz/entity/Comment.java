2b76eae9c002b29086f9325015cc36c9068055e1
ohyeongseok <dhgudtjr6635@gmail.com>
2024-07-15 16:40:15 +0900

    refactor/#23/comment 조회 최적화

--- src/main/java/lucky/seven/kanbanbuzz/entity/Comment.java
+++ src/main/java/lucky/seven/kanbanbuzz/entity/Comment.java
@@ -1,20 +1,15 @@
 package lucky.seven.kanbanbuzz.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "comments")
+@Table(name = "comments", indexes = {
+		@Index(name = "idx_comment_card", columnList = "card_id")
+})
 @NoArgsConstructor
 public class Comment extends Timestamped {
 	@Id
--- src/main/java/lucky/seven/kanbanbuzz/repository/CommentRepositroy.java
+++ src/main/java/lucky/seven/kanbanbuzz/repository/CommentRepositroy.java
@@ -3,10 +3,12 @@ package lucky.seven.kanbanbuzz.repository;
 import lucky.seven.kanbanbuzz.entity.Card;
 import lucky.seven.kanbanbuzz.entity.Comment;
 import lucky.seven.kanbanbuzz.entity.User;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepositroy extends JpaRepository<Comment,Long> {
+    @Cacheable(value = "comments", key = "#card.id + '-' + #pageable.pageNumber + '-' + #pageable.pageSize")
     Page<Comment> findByCard(Card card, Pageable pageable);
 }
